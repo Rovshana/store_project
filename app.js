1414514127d7d31c48d4c9599a155f2fbde2651a
Rovshana <rovshanarzayeva2001@gmail.com>
2022-09-27 23:51:36 +0400

    adding some functionality

--- app.js
+++ app.js
@@ -2,12 +2,106 @@
 let sliderWrapper = document.querySelector('.sliderWrapper')
 
 const list = document.querySelectorAll('li');
+// products
+const currentProductImg = document.querySelector('.productImg');
+        const productBrand = document.querySelector('.productBrand');
+        const productPrice = document.querySelector('.productPrice');
+        const productSizes = document.querySelectorAll('.size');
+        const productColors = document.querySelectorAll('.color');
+// product array
+let products = [
+    {id: 1,
+        brand: "Hippie",
+        price: 180,
+        colors: [{
+            code: "white",
+            img: "./images/white_shoe1.png"
+        },
+        {
+            code: "black",
+            img: "./images/nike-black1.png"
+        }
+    ]
+    },
+    {id: 2,
+        brand: "Blazer",
+        price: 119,
+        colors: [
+            {
+code: "black",
+img: "./images/black_shoe1.png"
+            },
+        {
+            code: "pink",
+            img: "./images/pink_blazer.png"
+        }
+        ]
+    },
+    {id: 3,
+        brand: "Air Force",
+        price: 130,
+        colors: [{
+            code: "gray",
+            img: "./images/sneaker1.png"
+
+        },
+            {
+            code: "white",
+            img: "./images/shoe_white.png"
+        }
+    ]
+
+    },
+    {id: 4,
+        brand: "Jordon",
+        price: 130,
+        colors:[{
+code: "blue",
+img: "./images/jordon1.png"
+        },
+            
+            {
+            code: "yellow",
+            img: "./images/yellow_shoe.png"
+        }
+    ]
+    }
+]
+
+let choosenProduct = products
 // slider start
 list.forEach((item, index)=>{
     item.addEventListener('click', ()=>{
         // console.log("uclicked" + index)
-        sliderWrapper.style.transform = `translateX(${-100 * index}vw)`
+        // current 
+        sliderWrapper.style.transform = `translateX(${-100 * index}vw)`;
+        // change chossen product
+        const choosenProduct = products[index]
+        console.log(choosenProduct)
+        
+        // change  text of product
+        productBrand.textContent = choosenProduct.brand;
+        productPrice.textContent = "$" + choosenProduct.price;
+        currentProductImg.src = choosenProduct.colors[0].img
+        console.log(currentProductImg)
+
+        // changeing color of product
+         productColors.forEach((item, index)=>{
+            item.style.backgroundColor = choosenProduct.colors[index].code
+         })
+
+
+
     })
 })
 
-//slider end
+// changing product color by clicking color of product
+productColors.forEach((color, index)=>{
+    console.log(index)
+    color.addEventListener('click', (index)=>{
+        currentProductImg.src = choosenProduct.colors[index].img
+        console.log(currentProductImg)
+
+    })
+})
+
