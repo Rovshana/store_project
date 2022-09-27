a3c56a428060de896c5b6ae2c535924cae36f47d
Rovshana <rovshanarzayeva2001@gmail.com>
2022-09-27 17:24:51 +0400

    features and product part has created

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 
 let sliderWrapper = document.querySelector('.sliderWrapper')
-const list = document.querySelectorAll('li');
 
+const list = document.querySelectorAll('li');
+// slider start
 list.forEach((item, index)=>{
     item.addEventListener('click', ()=>{
-        console.log("uclicked" + index)
-        sliderWrapper.style.transform = `translateX(${-100 * index} vw)`
+        // console.log("uclicked" + index)
+        sliderWrapper.style.transform = `translateX(${-100 * index}vw)`
     })
 })
+
+//slider end
